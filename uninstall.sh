95ac17b48be30408244f70c68f685350e766fad3
Russell Smith (Valorem Consulting) <v-rusmit@microsoft.com>
2024-01-21 10:57:28 -0600

    Fixing old name issue

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-teslaapi-evcharger')
+kill $(pgrep -f 'supervise dbus-grid-emulator')
 chmod a-x $SCRIPT_DIR/service/run
 ./restart.sh
